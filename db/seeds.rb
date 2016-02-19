9eea0622631d2c1e461e0ddcb96358d48896380f
Michael Zemel <michael.zemel@gmail.com>
2016-02-19 16:33:07 -0500

    Initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(
+	email: "test@example.com",
+	password: "password",
+	password_confirmation: "password"
+	)
+
+grade = Grade.create(
+	name: "V1",
+	value: 1,
+	)
+
+route = Route.create(
+	name: "Slippery Crack",
+	grade: grade
+	)
+
+project = Project.create(
+	route: route,
+	user: user
+	)
