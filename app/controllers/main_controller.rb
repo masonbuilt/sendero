275989460451bc6fa47ce7b297ca5e07643d93b7
Michael Zemel <michael.zemel@gmail.com>
2016-02-21 18:09:22 -0500

    Better approach

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,4 @@
 class MainController < ApplicationController
   def dashboard
-  	@routes = current_user.routes.limit(5) rescue []
-  	@projects = current_user.projects.limit(5) rescue []
-  	@comments = current_user.comments.limit(5) rescue []
   end
 end
