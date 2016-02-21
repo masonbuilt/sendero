11b84b4e692d0044138312c6657e579331016c1b
Michael Zemel <michael.zemel@gmail.com>
2016-02-21 13:20:28 -0500

    User should see complete Route information on a Project page

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,7 @@
 class Route < ActiveRecord::Base
 	belongs_to :grade
 	has_many :users, through: :projects
+	has_many :projects
 	has_many :comments
 	belongs_to :owner, class_name: "User", foreign_key: "owner_id"
 
