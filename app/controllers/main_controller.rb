d0f7037896534dc7e25dd5a419363fe7870e26c5
Michael Zemel <michael.zemel@gmail.com>
2016-02-21 17:53:29 -0500

    Improve dashboard

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def dashboard
+  	@routes = current_user.routes.limit(5)
+  	@projects = current_user.projects.limit(5)
+  	@comments = current_user.comments.limit(5)
   end
 end
