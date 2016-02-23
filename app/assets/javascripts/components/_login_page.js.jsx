da80fd97359640f0ce0b3164d50310c2bf9cd465
Jason Swetzoff <swetzoff@gmail.com>
2016-02-23 15:32:25 -0800

    Trying to get login to work.

--- app/assets/javascripts/components/_login_page.js.jsx
+++ app/assets/javascripts/components/_login_page.js.jsx
@@ -1,8 +1,6 @@
 var LoginPage = React.createClass({
   render: function() {
     return(<div>
-      <div className="aligner full-height">
-          <div className="login box theme--default aligner__item">
               <ul className="list--horiz">
                 <li className="list--horiz__item">
                   <a href="">Log In</a>
@@ -11,8 +9,36 @@ var LoginPage = React.createClass({
                   <a href="">Sign Up</a>
                 </li>
               </ul>
-          </div>
-      </div>
+              <form className="new_user" id="new_user" action="/users/sign_in" accept-charset="UTF-8" method="post">
+                <input name="utf8" type="hidden" value="✓" />
+                  <input type="hidden" name="" value=""/>
+                      <div className="form-element">
+                        <label className="form-element__label" for="user_email">Email</label>
+                        <div className="form-element__control">
+                          <input autofocus="autofocus" placeholder="email@sendero.com" type="email" value="" name="user[email]" id="user_email" />
+                        </div>
+                      </div>
+
+                      <div className="form-element">
+                        <label className="form-element__label" for="user_password">Password</label>
+                        <div className="form-element__control">
+                          <input autocomplete="off" placeholder="password" type="password" name="user[password]" id="user_password"/>
+                        </div>
+                      </div>
+
+                    <div className="form-element">
+                        <span className="checkbox">
+                          <input name="checkbox" type="hidden" value="0"/>
+                          <input className="checkbox__field" id="checkbox--remember" name="checkbox" type="checkbox" value="1" />
+                          <label className="checkbox__label" for="checkbox--remember">Remember me</label>
+                        </span>
+                    </div>
+
+                    <div className="form-element">
+                      <input type="submit" name="commit" value="Log in" className="btn--fit"/>
+                    </div>
+
+              </form>
     </div>);
   }
 });
